a2ebc13ea7d3451e22e554d233affd6ddefccc93
Nazgul28 <lfmassola@gmail.com>
2015-12-06 22:01:20 -0800

    update 06-12

--- wp-content/themes/kath_portfolio/js/main.js
+++ wp-content/themes/kath_portfolio/js/main.js
@@ -11,7 +11,7 @@ jQuery(document).ready(function($) {
         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
            if (target.length) {
              $('html,body').animate({
-                 scrollTop: target.offset().top - 650
+                 scrollTop: target.offset().top - 590
             }, 1000);
             return false;
         }
